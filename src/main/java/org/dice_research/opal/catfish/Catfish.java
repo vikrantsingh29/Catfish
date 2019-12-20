b6760c390011bd9556607b0b361ec16a228428d1
Gourab Sahu <gourab@mail.uni-paderborn.de>
2019-12-20 13:22:08 +0100

    Conflicts resolution

--- src/main/java/org/dice_research/opal/catfish/Catfish.java
+++ src/main/java/org/dice_research/opal/catfish/Catfish.java
@@ -1,34 +1,19 @@
 package org.dice_research.opal.catfish;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+// import org.apache.jena.rdf.model.ModelFactory;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
 import org.dice_research.opal.common.interfaces.JenaModelProcessor;
-import org.dice_research.opal.common.interfaces.ModelProcessor;
 
-@SuppressWarnings("deprecation")
-public class Catfish implements ModelProcessor, JenaModelProcessor {
+public class Catfish implements JenaModelProcessor {
 
-	private static final Logger LOGGER = LogManager.getLogger();
+	// private static final Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	public void processModel(Model model, String datasetUri) throws Exception {
-
-		// TODO: clean model
-
-		if (model.isEmpty()) {
-			LOGGER.warn("Model is empty.");
-		}
-
-	}
-
-	/**
-	 * @deprecated Replaced by {@link #processModel(Model, String)}.
-	 */
-	@Deprecated
-	@Override
 	public Model process(Model model, String datasetUri) throws Exception {
-		processModel(model, datasetUri);
-		return model;
+
+		ModelHeterogeneousLicenseCleaner LicenseCleaner = new ModelHeterogeneousLicenseCleaner();
+		return LicenseCleaner.ModelLicenCleaner(model,datasetUri);
 	}
+
 }
